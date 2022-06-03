678765750d5f28d3816c255c300d940f115d0633
prameshmca5 <rameshkp2@gmail.com>
2022-06-03 18:13:56 +0800

    Testcommit

---
+++ src/main/java/com/ramesh/dockspringboot/DockspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ramesh.dockspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockspringbootApplication.class, args);
+	}
+
+}
